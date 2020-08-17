46a61866269112d271fc4a330d40138f427a1a81
amitsingh <amitsingh98.@72.com>
2020-08-17 15:52:50 +0530

    Updates

---
+++ precache-manifest.751ced76284f4c5061a0f718f3943601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad3d3f8d51ff9efa09819dce028ab31",
+    "url": "/new-calculater/index.html"
+  },
+  {
+    "revision": "00cbf66de9a84f93c394",
+    "url": "/new-calculater/static/css/main.0daff014.chunk.css"
+  },
+  {
+    "revision": "fc4efc6db883aef968d5",
+    "url": "/new-calculater/static/js/2.ffb6248d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-calculater/static/js/2.ffb6248d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cbf66de9a84f93c394",
+    "url": "/new-calculater/static/js/main.900165c8.chunk.js"
+  },
+  {
+    "revision": "e4fcd801bd1221c367d2",
+    "url": "/new-calculater/static/js/runtime-main.c0fa82ce.js"
+  }
+]);
